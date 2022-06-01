6428528d6e3d0fd41436027ecd79fd8abdb48e88
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-06-01 22:48:12 +0530

    first commit

---
+++ src/components/IT20127046/Home.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import HomeImage from "../../../public/images/HomeImage.jpg";
+import RightSidePanel from "./RightSidePanel";
+import jwt_decode from "jwt-decode";
+import SLIITResearchLogo from '../../../public/images/SLIITResearchLogo.png'
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userType: "",
+    };
+  }
+
+  componentDidMount() {
+    document.title = "Home";
+
+    // Check user login
+    if (!localStorage.userToken) {
+      swal({
+        title: "Please login first",
+        icon: "warning",
+      }).then((value) => {
+        if (value) {
+          this.props.history.push("/user/login");
+          window.location.reload();
+        }
+      });
+    } else {
+      const usertoken = localStorage.userToken;
+      const decoded = jwt_decode(usertoken);
+      this.setState({
+        userType: decoded.type,
+      });
+    }
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <br />
+          <div className="container">
+            <h4><i class="fa fa-home" aria-hidden="true"></i>&nbsp;&nbsp;Home</h4>
+            <hr />
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-9">
+                <div>
+                  <img
+                    src={HomeImage}
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      marginBottom: "20px",
+                    }}
+                  />
+                </div>
+                <div>
+                  <div className="p-3 mb-2 bg-light text-dark">
+                    <p className="h6">
+                      <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                      &nbsp;&nbsp;Notice
+                    </p>
+                    <hr />
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="container">
+                <center>
+                    <img
+                      src={SLIITResearchLogo}
+                      style={{ width: "100px", height: "100px" }}
+                    />
+                  </center>
+                </div>
+                <RightSidePanel />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/IT20127046/admin/DocumentTemp_Admin.js
@@ -0,0 +1,161 @@
+import React, { Component } from "react";
+import axios from "axios";
+import fileDownload from "js-file-download";
+import AdminNavBar from "../../IT20125202/admin/AdminNavBar";
+import swal from "sweetalert";
+
+export default class DocumentTemp_Admin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      documentTemp: [],
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveDocumentTemp();
+  }
+
+  // To get all data
+  retrieveDocumentTemp() {
+    axios.get("http://localhost:5000/template/getAll").then((res) => {
+      if (res.data.success) {
+        this.setState({
+          documentTemp: res.data.exsitingDocumentTemp,
+        });
+        console.log(this.state.documentTemp);
+      }
+    });
+  }
+
+  // To delete data
+  onDelete = (id) => {
+    swal({
+        title: "Are you sure?",
+        text: "Delete this Document",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+  
+          axios.delete(`http://localhost:5000/document/delete/${id}`).then((res) => {
+              swal("Deleted Successfull!", {
+                  icon: "success",
+              });
+  
+              setTimeout(() => {
+                this.retrieveDocumentTemp();
+              }, 1000);
+          }); 
+        } else {}
+      });
+
+    // axios.delete(`http://localhost:5000/document/delete/${id}`).then((res) => {
+    //   alert("Delete Successful");
+    //   this.retrieveDocumentTemp();
+    // });
+  };
+
+  // Download File
+  downloadFile = (fileName) => {
+    const data = {
+      fileName: fileName,
+    };
+
+    axios({
+      url: "http://localhost:5000/file/download",
+      data,
+      method: "POST",
+      responseType: "blob",
+    }).then((res) => {
+      console.log(res);
+      fileDownload(res.data, fileName);
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <AdminNavBar />
+        <br />
+        <h4>Document Template</h4>
+        <hr />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <div>
+                <center>
+                  <a
+                    className="btn btn-outline-success m-2"
+                    href="/add/documentTemp"
+                  >
+                    Add Template
+                  </a>
+                </center>
+              </div>
+            </div>
+            <div className="col-9">
+              <div className="container">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Document Type</th>
+                      <th scope="col">Other Type</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">File</th>
+                      <th scope="col">Preview</th>
+                      <th scope="col">Update</th>
+                      <th scope="col">Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.documentTemp.map((documentTemp, index) => (
+                      <tr key={index}>
+                        <th scope="row">{index + 1}</th>
+                        <td>{documentTemp.documentType}</td>
+                        <td>{documentTemp.otherType}</td>
+                        <td>{documentTemp.description}</td>
+                        <td>{documentTemp.files}</td>
+                        <td>
+                          <a
+                            className="btn btn-outline-primary m-2"
+                            onClick={() =>
+                              this.downloadFile(documentTemp.files)
+                            }
+                          >
+                            Download
+                          </a>
+                        </td>
+                        <td>
+                          <a
+                            className="btn btn-outline-success m-2"
+                            href={`/edit/documentTemp/${documentTemp._id}`}
+                          >
+                            Update
+                          </a>
+                        </td>
+                        <td>
+                          <a
+                            className="btn btn-outline-danger m-2"
+                            onClick={() => this.onDelete(documentTemp._id)}
+                          >
+                            Delete
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
